4c0bf4e7f55b7c069dedc9604137ec9149e2f09f
xo58gole <148429124+xo58gole@users.noreply.github.com>
2023-11-13 19:14:09 +0100

    Update exercise1.py

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -1,43 +1,56 @@
-import pandas as pd
-from sqlalchemy import create_engine
-from sqlalchemy import text
 import csv
-from io import StringIO
+import requests
+from sqlite3 import connect
 
-url = "csv.csv"
-data = []
+# Read CSV file from URL
+url = "https://opendata.rhein-kreis-neuss.de/api/v2/catalog/datasets/rhein-kreis-neuss-flughafen-weltweit/exports/csv"
+response = requests.get(url)
+decoded_content = response.content.decode('utf-8')
+csv_reader = csv.reader(decoded_content.splitlines(), delimiter=',')
+header = next(csv_reader)  # get header row
+print(f"Number of columns: {len(header)}")
 
-# Read the file line by line, handling any problematic lines
-with open(url, 'r', encoding='utf-8') as file:
-    reader = csv.reader(file)
-    for line in reader:
-        try:
-            line_str = ",".join(line)
-            data.append(pd.read_csv(StringIO(line_str), header=None))
-        except pd.errors.ParserError:
-            pass  # Skip problematic lines
+# Define SQLite database connection and create table
+conn = connect("airports.sqlite")
+cursor = conn.cursor()
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS airports (
+        column_1 INTEGER PRIMARY KEY,
+        column_2 TEXT,
+        column_3 TEXT,
+        column_4 TEXT,
+        column_5 TEXT,
+        column_6 TEXT,
+        column_7 FLOAT,
+        column_8 FLOAT,
+        column_9 FLOAT,
+        column_10 TEXT,
+        column_11 TEXT,
+        column_12 TEXT,
+        geo_punkt TEXT
+    )
+""")
 
-# Concatenate the data into a DataFrame
-df = pd.concat(data)
+# Write data to SQLite table
+for row in csv_reader:
+    cursor.execute("""
+        INSERT INTO airports (
+            column_1,
+            column_2,
+            column_3,
+            column_4,
+            column_5,
+            column_6,
+            column_7,
+            column_8,
+            column_9,
+            column_10,
+            column_11,
+            column_12,
+            geo_punkt
+        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    """, row)
 
-engine = create_engine("sqlite:///airports.sqlite")
-with engine.connect() as conn:
-    conn.execute(text("""
-        CREATE TABLE IF NOT EXISTS airports (
-            column_1 INTEGER PRIMARY KEY,
-            column_2 TEXT,
-            column_3 TEXT,
-            column_4 TEXT,
-            column_5 TEXT,
-            column_6 TEXT,
-            column_7 FLOAT,
-            column_8 FLOAT,
-            column_9 FLOAT,
-            column_10 TEXT,
-            column_11 TEXT,
-            column_12 TEXT,
-            geo_punkt TEXT
-        )
-    """))
-
-    df.to_sql("airports", engine, if_exists="replace", index=False)
+# Commit changes and close connection
+conn.commit()
+conn.close()
