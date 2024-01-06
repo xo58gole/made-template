b35a6b185ec9583a779f222877466d39ad0dc3b7
xo58gole <148429124+xo58gole@users.noreply.github.com>
2024-01-06 14:50:52 +0100

    Update testProj4.py

--- project/testProj4.py
+++ project/testProj4.py
@@ -4,9 +4,11 @@ import os
 
 class TestDataPipeline(unittest.TestCase):
     def test_data_pipeline(self):
+        # Add the command to change the permission of the script
+        os.system('chmod +x ./pipeline.sh')
         # Execute the data pipeline
         os.system('./pipeline.sh')
-        print("Success: Data pipeline executed")
+        print("Success: Data pipeline exists")
 
 class TestFilesAndColumns(unittest.TestCase):
     def test_data_files_exist(self):
